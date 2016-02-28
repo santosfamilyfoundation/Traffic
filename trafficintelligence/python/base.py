bfb2b6d7b0d37239917d0d2fc2b34d7a07649491
Samantha Kumarasena <samantha.kumarasena@students.olin.edu>
2016-02-28 18:55:54 -0500

    Adding my comments to my assigned files.

--- trafficintelligence/python/base.py
+++ trafficintelligence/python/base.py
@@ -1,5 +1,16 @@
 '''Module for few base classes to avoid issues of circular import'''
 
+'''
+Sam comments:
+This object is found in:
+     - events.py
+     - moving.py
+     - storage.py
+     
+It seems to be a class that is extended upon to allow the ability to add a video filename. 
+
+'''
+
 class VideoFilenameAddable(object):
     'Base class with the capability to attach a video filename'
 
